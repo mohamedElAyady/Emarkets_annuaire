61edea824d0a6221aa7655f17dcd696cb8bc8b53
mehdi outmamant <outmamantmehdi@gmail.com>
2023-05-11 12:53:41 +0100

    acceuil task

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,12 +3,13 @@
       <!-- Favicons -->
       <link href="assets/annuaire/img/favicon.png" rel="icon">
       <link href="assets/annuaire/img/apple-touch-icon.png" rel="apple-touch-icon">
+      
   
       <!-- Google Fonts -->
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   
       <!-- Scripts -->
-      @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+     
   
       <!-- Template Main CSS File -->
       <link href="{{ asset('assets/annuaire/css/main.css') }}" rel="stylesheet"> 
@@ -31,7 +32,22 @@
           <li><a class="nav-link scrollto" href="#services">Blog</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">Contacter-nous</a></li>
           
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          <li>
+            @if (Route::has('login'))
+            <div class="switch">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <li><a href="{{ route('login') }}" class="getstarted " >Log in</a></li>
+
+                    @if (Route::has('register'))
+                       <li> <a href="{{ route('register') }}" class=" getstarted" >Register</a></li>
+                    @endif
+                @endauth
+
+            </div>
+             @endif
+           </li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
